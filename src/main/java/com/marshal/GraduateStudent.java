a9e3f44b36c336632f19aba54f369af0217625fe
Marshal <ga031243@gmail.com>
2022-03-11 12:54:25 +0800

    2022/3/11

---
+++ src/main/java/com/marshal/GraduateStudent.java
@@ -0,0 +1,7 @@
+package com.marshal;
+
+public class GraduateStudent extends Student {
+    public GraduateStudent(String name){
+        super(name);
+    }
+}
--- src/main/java/com/marshal/Student.java
+++ src/main/java/com/marshal/Student.java
@@ -1,13 +1,29 @@
 package com.marshal;
 
 public class Student {
-    public String name;
-    public int english;
-    public int math;
+    String name;
+    int english;
+    int math;
 
-    public static void main(String[] args) {
-            String name;
-            int english;
-            int math;
+    public Student(String name){ //Constructor
+        this.name=name;
+    }
+    public Student(String name, int english, int math) {
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+    public Student() { //Default Constructor
+
+    }
+    public void print(){
+        int average = (english+math)/2;
+        System.out.print(name + "\t" + english +
+                "\t" +math + "\t" + average);
+        if(average < 60){
+            System.out.print("*");
+        }
+        System.out.println();
+
         }
     }
