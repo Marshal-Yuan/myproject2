f6eeb1392322436163dbc1083862531df6b90270
Marshal <ga031243@gmail.com>
2022-05-27 12:53:48 +0800

    2022/5/27 12:53

--- src/main/java/com/marshal/ui/GuessFrame.java
+++ src/main/java/com/marshal/ui/GuessFrame.java
@@ -7,25 +7,39 @@ import java.awt.event.ActionListener;
 
 public class GuessFrame extends JFrame {
 
+    JButton button = new JButton("Guess");
+    JLabel label = new JLabel("Zzzz.");
+    JTextField number = new JTextField(10);
+
     public GuessFrame(){
         super();
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button = new JButton("Hi");
 
-        JLabel label = new JLabel("Zzzz.");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 //                System.out.println("Hello");
-                label.setText("Hello!!");
+//                label.setText("Hello!!");
+                int num =Integer.parseInt(number.getText());
+                System.out.println(num);
+                if(num == 15) {
+                    label.setText("恭喜你猜對了!!");
+                }
+                else if(num < 15){
+                        label.setText("再大一點");
+                    }
+                else{
+                    label.setText("再小一點");
+                }
             }
         });
 
         setLayout(new FlowLayout());
-        add(label);
+        add(number);
         add(button);
+        add(label);
         setVisible(true);
 
     }
