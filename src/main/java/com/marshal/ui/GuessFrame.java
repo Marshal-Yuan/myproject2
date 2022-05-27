ad916ce0c94a5932e04a29059ce7f725709fcea4
Marshal <ga031243@gmail.com>
2022-05-27 13:40:03 +0800

    2022/5/27 13:30

--- src/main/java/com/marshal/ui/GuessFrame.java
+++ src/main/java/com/marshal/ui/GuessFrame.java
@@ -4,11 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
 
     JButton button = new JButton("Guess");
-    JLabel label = new JLabel("Zzzz.");
+    JLabel label = new JLabel("請在1~30間猜數字");
     JTextField number = new JTextField(10);
 
     public GuessFrame(){
@@ -16,6 +17,8 @@ public class GuessFrame extends JFrame {
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        Random rand = new Random();
+        int secret = rand.nextInt(30) + 1;
 
         button.addActionListener(new ActionListener() {
             @Override
@@ -24,14 +27,19 @@ public class GuessFrame extends JFrame {
 //                label.setText("Hello!!");
                 int num =Integer.parseInt(number.getText());
                 System.out.println(num);
-                if(num == 15) {
-                    label.setText("恭喜你猜對了!!");
+
+
+                if(num == secret) {
+                    label.setText("恭喜你猜對了!!數字是"+ secret);
                 }
-                else if(num < 15){
+                else if(num > 0  && num < secret){
                         label.setText("再大一點");
                     }
-                else{
+                else if(num <31 && num > secret) {
                     label.setText("再小一點");
+                }
+                else{
+                    label.setText("超出範圍了，請重新輸入!!");
                 }
             }
         });
